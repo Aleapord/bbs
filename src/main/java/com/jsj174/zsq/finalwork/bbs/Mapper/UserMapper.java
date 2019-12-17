60a5902b067c52ff5d0371f4480502274b1445d2
Amor-Joo <hakim15170612044@outlook.com>
2019-12-17 20:31:10 +0800

    测试IDEA里的GIT提交

--- src/main/java/com/jsj174/zsq/finalwork/bbs/Mapper/UserMapper.java
+++ src/main/java/com/jsj174/zsq/finalwork/bbs/Mapper/UserMapper.java
@@ -1,4 +1,17 @@
 package com.jsj174.zsq.finalwork.bbs.Mapper;
 
+import com.jsj174.zsq.finalwork.bbs.Models.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Mapper
+@Component
 public interface UserMapper {
+
+    @Select("select * from user")
+    List<User> users();
+
 }
