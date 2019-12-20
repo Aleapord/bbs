7ff00fea4ee44ab8a0bc4083fa74b224b0baf904
zhui177 <6109117169@email.ncu.edu.cn>
2019-12-20 21:32:44 +0800

    UserService

--- src/main/java/com/jsj174/zsq/finalwork/bbs/Services/UserService.java
+++ src/main/java/com/jsj174/zsq/finalwork/bbs/Services/UserService.java
@@ -5,19 +5,28 @@ import com.jsj174.zsq.finalwork.bbs.Models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
     @Autowired
     private UserMapper userMapper;
 
+    public void insertUser(User user){
+        userMapper.insertUser(user);
+    }
     public User getUser(int id){
         return userMapper.getUserById(id);
     }
     public User getUser(String name){
         return userMapper.getUserByName(name);
     }
+    public List<User> getAllUser() {
+
+        return userMapper.getAllUser();
+    }
     public void deleteUser(User user){
-        userMapper.deleteUser(user.getId());
+        userMapper.deleteUser(user.getUserID());
     }
 }
