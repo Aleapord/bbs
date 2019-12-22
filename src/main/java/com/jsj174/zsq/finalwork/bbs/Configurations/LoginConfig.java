d89d17a298005f92f387387d366a035e8ce60ed7
zhui177 <6109117169@email.ncu.edu.cn>
2019-12-22 17:11:29 +0800

    添加采纳、回帖时间

--- src/main/java/com/jsj174/zsq/finalwork/bbs/Configurations/LoginConfig.java
+++ src/main/java/com/jsj174/zsq/finalwork/bbs/Configurations/LoginConfig.java
@@ -5,17 +5,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class LoginConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor())
-                .addPathPatterns("/**");
+        registry.addInterceptor(loginInterceptor()).addPathPatterns("/**");
     }
 
     @Bean
-    public LoginInterceptor loginInterceptor(){
+    public LoginInterceptor loginInterceptor() {
         return new LoginInterceptor();
     }
 }
--- src/main/java/com/jsj174/zsq/finalwork/bbs/Controller/UserController.java
+++ src/main/java/com/jsj174/zsq/finalwork/bbs/Controller/UserController.java
@@ -1,6 +1,5 @@
 package com.jsj174.zsq.finalwork.bbs.Controller;
 
-import com.jsj174.zsq.finalwork.bbs.Models.Post;
 import com.jsj174.zsq.finalwork.bbs.Services.CommentService;
 import com.jsj174.zsq.finalwork.bbs.Services.PostService;
 import com.jsj174.zsq.finalwork.bbs.Services.UserService;
@@ -26,14 +25,14 @@ public class UserController {
 
     @PostMapping("/getWealth")
     @ResponseBody
-    public HashMap<String,Object> getWealth(@PathVariable int userID){
-        HashMap<String,Object> hashMap = new HashMap<>();
+    public HashMap<String, Object> getWealth(@PathVariable int userID) {
+        HashMap<String, Object> hashMap = new HashMap<>();
 
-        if(userService.getUser(userID) == null)
-            hashMap.put("msg","该用户不存在！");
+        if (userService.getUser(userID) == null)
+            hashMap.put("msg", "该用户不存在！");
         else {
             int wealth = userService.getWealth(userID);
-            hashMap.put("wealth",wealth);
+            hashMap.put("wealth", wealth);
         }
         return hashMap;
     }
